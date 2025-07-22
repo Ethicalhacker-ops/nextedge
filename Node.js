199d9e7634dd58952846e77ab9718ab7c8ac1b85
The Bijay <138951312+Ethicalhacker-ops@users.noreply.github.com>
2025-07-22 07:18:56 +0545

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,13 @@
+backend/
+├── server.js
+├── routes/
+│   ├── api.js
+│   └── contact.js
+├── controllers/
+│   ├── contactController.js
+│   └── emailController.js
+├── models/
+│   └── Contact.js
+├── config/
+│   └── db.js
+└── package.json
